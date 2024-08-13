f515477e42e29c9718ad34d09c79ef7fa990ecba
axuanxuanxuanxuan <2010106312@qq.com>
2024-08-13 14:00:02 +0800

    xindeng1

---
+++ xindeng1/xindeng.c
@@ -0,0 +1,48 @@
+#include"REG52.h"
+#include"intrins.h"
+sbit LED0=P0^0;
+sbit LED1=P0^1;
+sbit LED2=P0^2;
+sbit LED3=P0^3;
+sbit LED4=P0^4;
+sbit LED5=P0^5;
+sbit LED6=P0^6;
+sbit LED7=P0^7;
+sbit LED8=P1^0;
+sbit LED9=P1^1;
+sbit LED10=P1^2;
+sbit LED11=P1^3;
+sbit LED12=P1^4;
+sbit LED13=P3^5;
+sbit LED14=P1^6;
+sbit LED15=P1^7;
+sbit LED16=P2^0;
+sbit LED17=P2^1;
+sbit LED18=P2^2;
+sbit LED19=P2^3;
+sbit LED20=P2^4;
+sbit LED21=P2^5;
+sbit LED22=P2^6;
+sbit LED23=P2^7;
+sbit LED24=P0^0;
+sbit LED25=P3^1;
+sbit LED26=P3^2;
+sbit LED27=P3^3;
+sbit LED28=P3^4;
+sbit LED29=P3^5;
+sbit LED30=P3^6;
+sbit LED31=P3^7;
+sbit LED32=P4^0;
+sbit LED33=P4^1;
+sbit LED34=P4^2;
+sbit LED35=P4^3;
+sbit LED36=P4^4;
+sbit LED37=P4^5;
+sbit LED38=P4^6;
+sbit LED39=P4^7;
+void main()
+{   
+
+
+
+}
